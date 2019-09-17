214bc4d75838e5aab10a412f2cebe6b64847b76e
Magnus Breki <breki.magnusson@gmail.com>
2019-09-17 09:13:00 +0000

    added instructions

--- tile_travel.py
+++ tile_travel.py
@@ -1,1 +1,6 @@
-# code
+# 1  make variables for the directions and x, y coordinates
+# 2  find out out which direction he can take
+# 3  ask which for direction input
+# 4  move the player
+# 5  if the player is on tile 3, 1 the player wins 
+# 6  else continue
