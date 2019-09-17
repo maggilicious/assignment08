0dad0f7c9644be65d77d70078cc8bca0779555b6
Bjarki Snær Magnússon <bjarkim@ru.is>
2019-09-17 09:18:23 +0000

    added skeleton

--- tile_travel.py
+++ tile_travel.py
@@ -4,3 +4,18 @@
 # 4  move the player
 # 5  if the player is on tile 3, 1 the player wins 
 # 6  else continue
+
+#TODO:
+# Make variables for south, north, east, west, x and y
+
+#TODO:
+#Make a function that return the options available
+
+#TODO:
+# Ask for input and save it
+
+#TODO:
+# Make a function that moves the player
+
+#TODO:
+#Check if the player is in a winning position
